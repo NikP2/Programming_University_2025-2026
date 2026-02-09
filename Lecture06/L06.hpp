16d544e55ad32f848cfc879a99e903bb081848bf
Nikita <nkt@MacBook-Air-Nikita.local>
2026-02-09 11:58:56 +0200

    A

--- Lecture06/L06.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace mt {
-    extern int n; //создаёт по сути global premium переменную, которая распространяет своё влияние на другие файлы
-    void foo();
-}
--- Lecture06/L06_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "L06.hpp"
-
-int main() {
-    std::cout << mt::n << std::endl;
-    mt::n = 20;
-    std::cout << mt::n << std::endl;
-    return 0;
-}
